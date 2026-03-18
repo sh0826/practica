9d2774787549fd3899148be66ec81f08f9e4830c
SHARON HERNANDEZ <sharon.dp.hernandez@gmail.com>
2026-03-18 14:09:44 -0500

    create

--- CRUDS/producto/models.py
+++ CRUDS/producto/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Producto (models.Model):
+    nombre = models.CharField(max_length=20)
+    tipo_Producto = [
+        ('cerveza', 'Cerveza'),
+        ('licor', 'Licor'),
+        ('cigarrillo', 'Cigarrillos')
+    ]
+    stock = models.IntegerField()
+    precio_uni = models.IntegerField()
+    imagen = models.ImageField(upload_to="ProductoApp")
