4b995b9011b29d88e6859d6f13a89018416de7e4
SHARON HERNANDEZ <sharon.dp.hernandez@gmail.com>
2026-03-18 14:23:22 -0500

    subit

--- CRUDS/producto/models.py
+++ CRUDS/producto/models.py
@@ -2,18 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Producto (models.Model):
-    nombre = models.CharField(max_length=20)
     tipo_Producto = [
         ('cerveza', 'Cerveza'),
         ('licor', 'Licor'),
         ('cigarrillo', 'Cigarrillos')
     ]
+    nombre = models.CharField(max_length=20)
+    tipo = models.CharField(max_length=30, choices=tipo_Producto)
     stock = models.IntegerField()
     precio_uni = models.IntegerField()
     imagen = models.ImageField(upload_to="ProductoApp")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
     
     class Meta:
         verbose_name = "Producto"
         verbose_name_plural = "Productos"
     def __str__(self):
-        return self.tipo_Producto
+        return self.nombre
