0aa23636db9705852c9d36fe0e669c7aa22194f5
SHARON HERNANDEZ <sharon.dp.hernandez@gmail.com>
2026-03-18 14:18:19 -0500

    add

--- CRUDS/producto/models.py
+++ CRUDS/producto/models.py
@@ -11,3 +11,9 @@ class Producto (models.Model):
     stock = models.IntegerField()
     precio_uni = models.IntegerField()
     imagen = models.ImageField(upload_to="ProductoApp")
+    
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
+    def __str__(self):
+        return self.tipo_Producto
