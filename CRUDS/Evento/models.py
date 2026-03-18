5773307fd53b1e02a999675f331aa75161e0b175
Paulac25 <condepaula472@gmail.com>
2026-03-18 14:22:15 -0500

    Add: modelo evento

--- CRUDS/Evento/models.py
+++ CRUDS/Evento/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Evento(models.Model):
+    nombre = models.CharField(max_length=100)
+    capacidad_maxima = models.IntegerField()
+    descripcion = models.CharField(max_length=155)
+    fecha = models.DateTimeField(auto_now_add=True)
+    hora_inicio = models.TimeField(auto_now_add=True)
+    precio_boleta = models.IntegerField()
+    imagen = models. ImageField(upload_to="EventosApp")
