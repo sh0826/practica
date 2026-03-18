2ad26bdb6446189fef6f1581a844f60f44157c12
Paulac25 <condepaula472@gmail.com>
2026-03-18 15:01:49 -0500

    Add: eventos

--- CRUDS/Evento/admin.py
+++ CRUDS/Evento/admin.py
@@ -3,3 +3,6 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.Evento)
+admin.site.site_header = "Gastro Bar"
+admin.site.site_title = "Agora - Admin "
+admin.site.index_title = "Bienvenido"
